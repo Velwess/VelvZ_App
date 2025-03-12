750c8caf846e6f72558afeeeb929648089cb7059
Salathiel <salathiel@genese.name>
2025-03-12 19:39:57 +0100

    feat: add sorting & persist its state through URL
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/pages/Offers.tsx
+++ src/pages/Offers.tsx
@@ -1,15 +1,29 @@
 import {useEffect, useState} from 'react';
-import {Link, useSearchParams} from 'react-router-dom';
-import {Star} from 'lucide-react';
+import {Link, useNavigate, useSearchParams} from 'react-router-dom';
+import {Filter, Star} from 'lucide-react';
 import {FavoriteButton} from '../components/FavoriteButton';
 import {Category, Deal} from "../lib/database.types.ts";
 import {supabase} from "../lib/supabase.ts";
 
+interface Sort {
+  field: string;
+  label: string;
+  ascending?: boolean;
+}
+
+const SORTS: Sort[] = [
+  {field: '', label: 'Trier'},
+  {field: 'final_price', ascending: true, label: 'Prix Croissant'},
+  {field: 'final_price', ascending: false, label: 'Prix Décroissant'},
+];
+
 function Offers() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [deals, setDeals] = useState<Deal[]>([]);
-  const category_slug = searchParams.get('category');
+  const sort_by = searchParams.get('sort_by') ?? '';
   const [category, setCategory] = useState<Category>();
+  const category_slug = searchParams.get('category') ?? '';
 
   useEffect(() => {
     supabase.from('categories').select().eq('slug', category_slug).maybeSingle()
@@ -17,18 +31,33 @@ function Offers() {
   }, [category_slug]);
   useEffect(() => {
     let query = supabase.from('deals').select();
-    if (category) query = query.eq('category_id', category.id)
+    const sort = SORTS.find(({label}) => label === sort_by);
+    if (category) query = query.eq('category_id', category.id);
+
     query.gte('end_date', new Date().toISOString())
-      .order('end_date', {ascending: false})
+      .order(sort?.field ?? 'end_date', {ascending: !!sort?.ascending})
       .then(({data}) => setDeals(data as Deal[]), console.error);
-  }, [category?.id]);
+  }, [sort_by, category?.id]);
 
   return (
     <div className="space-y-8">
-      <h2 className="text-3xl font-bold text-gray-800">Toutes les offres</h2>
+
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-bold text-gray-800">Toutes les offres</h2>
+        <div className="flex items-center space-x-4">
+          <Filter className="text-[#E6A4B4]" size={20}/>
+          <select
+            value={sort_by as string}
+            onChange={(e) => navigate(`/offres?category=${category_slug}${(_ => 'Trier' === _ ? '' : `&sort_by=${_}`)(e.target.value)}`)}
+            className="border border-gray-200 rounded-full px-4 py-2 text-gray-600 focus:outline-none focus:border-[#E6A4B4]"
+          >
+            {SORTS.map(({label}) => <option value={label} key={label}>{label}</option>)}
+          </select>
+        </div>
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {deals.map((deal) => (
+        {deals?.map((deal) => (
           <Link
             to={`/offre/${deal.id}`}
             key={deal.id}
