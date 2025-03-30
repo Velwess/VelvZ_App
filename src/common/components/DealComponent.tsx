4d3a8003d692b61901e245cd7729138c9ea98ad8
Salathiel <salathiel@genese.name>
2025-03-30 23:59:42 +0200

    feat: show a deal description preview
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/common/components/DealComponent.tsx
+++ src/common/components/DealComponent.tsx
@@ -10,7 +10,7 @@ export function DealComponent({deal}: { deal: Deal }) {
   const router = useRouter();
 
   return <div onClick={() => router.push(`/offres/${deal.id}`)}
-              className="relative bg-white rounded-xl shadow-sm cursor-pointer overflow-hidden hover:shadow-md transition-shadow">
+              className="flex flex-col relative bg-white rounded-xl shadow-sm cursor-pointer overflow-hidden hover:shadow-md transition-shadow">
     {deal.opinion && <div
       className="z-[1] px-5 py-1 flex gap-1.5 text-xs bottom-full absolute text-white drop-shadow-lg place-items-center bg-[#E6A4B4] -rotate-45 origin-bottom-right -translate-x-[calc(29.28%)]">
       On like &#x2764;
@@ -21,7 +21,7 @@ export function DealComponent({deal}: { deal: Deal }) {
         {deal.discount_percentage ? `-${deal.discount_percentage}%` : 'Bon Plan'}
       </span>
     </div>
-    <div className="p-6">
+    <div className="p-6 grow flex flex-col border">
       <div className="flex items-center justify-between mb-2">
         <Link href={`/offres?categorie=${deal.categories?.slug}`}
               className="text-sm text-[#E6A4B4]">{deal.categories?.name}</Link>
@@ -33,12 +33,16 @@ export function DealComponent({deal}: { deal: Deal }) {
         </div>
       </div>
       <h4 className="text-xl font-semibold text-gray-800 mb-2">{deal.title}</h4>
+      <hr className="grow opacity-0"/>
       <div className="flex items-center justify-between">
         <span className="text-sm text-gray-500">
           {deal.end_date && `Expire le ${new Date(deal.end_date).toLocaleDateString('fr')}` || ''}
         </span>
         <FavoriteButton id={deal.id}/>
       </div>
+      <p className="opacity-60 text-gray-600 text-ellipsis overflow-hidden whitespace-nowrap" >
+        {deal.description ?? null}
+      </p>
     </div>
   </div>;
 }
