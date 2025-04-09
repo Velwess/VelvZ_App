ee6027e28d040f6d5e59de4b5eea36fabc1bff7e
Salathiel <salathiel@genese.name>
2025-04-09 14:57:43 +0200

    feat: extract footer as its own component
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import {FavouriteDealIdsContext, PageSizeContext, SessionContext, UserContext} from "@velz/common/domain/context.ts";
-import {Facebook, Heart, Instagram, Mail, Phone, Power, Sparkles, Twitter, UserRoundPlus} from "lucide-react";
-import {usePathname, useRouter, useSearchParams} from "next/navigation";
 import {ApiResponse, Category} from "@velz/common/lib/database.types.ts";
 import {Navbar} from "@velz/common/components/nav-bar.tsx";
+import {Footer} from "@velz/common/components/footer.tsx";
 import {ReactNode, useEffect, useState} from "react";
 import {Session, User} from "@supabase/supabase-js";
 import favicon from "@velz/assets/logo.png";
-// import favicon from '@velz/favicon.png';
-import Link from "next/link";
 import "@velz/index.css";
-import {z} from "zod";
 
 export interface RootClientLayoutProps {
   favouriteDealIds?: string[];
@@ -20,12 +16,7 @@ export interface RootClientLayoutProps {
 }
 
 export default function RootClientLayout(props: RootClientLayoutProps) {
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
   const [pageSize, setPageSize] = useState(20);
-  const [newsLetterEmail, setNewsLetterEmail] = useState('');
-  const [invalidNewsLetterEmail, setInvalidNewsLetterEmail] = useState(true);
   const [categories, setCategories] = useState<Category[]>(props.categories ?? []);
   const [user, setUser] = useState<User | null | undefined>(
     JSON.parse('object' === typeof localStorage
@@ -53,8 +44,6 @@ export default function RootClientLayout(props: RootClientLayoutProps) {
     localStorage?.setItem('favouriteDealIds', content);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [favouriteDealIds?.sort().join()]);
-  useEffect(() =>
-    setInvalidNewsLetterEmail(!!z.string().email().safeParse(newsLetterEmail).error), [newsLetterEmail]);
 
   return <html lang="fr">
   <head>
@@ -77,127 +66,10 @@ export default function RootClientLayout(props: RootClientLayoutProps) {
             {props.children}
           </main>
 
-          {/* Footer */}
-          <footer className="bg-white border-t border-gray-100">
-            <div className="container mx-auto px-4 py-12">
-              <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-                <div>
-                  <h4 className="text-lg font-semibold text-gray-800 mb-4">
-                    À propos
-                  </h4>
-                  <ul className="space-y-2 text-gray-600">
-                    <li>
-                      <Link href="/vision" className="hover:text-[#E6A4B4] transition-colors">
-                        Notre Vision
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="/engagement" className="hover:text-[#E6A4B4] transition-colors">
-                        Notre engagement
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="/mission" className="hover:text-[#E6A4B4] transition-colors">
-                        Notre mission
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="/histoire" className="hover:text-[#E6A4B4] transition-colors">
-                        Notre Histoire
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-                  <h4 className="text-lg font-semibold text-gray-800 mb-4">Aide</h4>
-                  <ul className="space-y-2 text-gray-600">
-                    <li>
-                      <Link href="/cgu" className="hover:text-[#E6A4B4] transition-colors">
-                        Conditions générales d'utilisation{" "}
-                      </Link>
-                    </li>
-
-                    <li>Cookies</li>
-                    <li>Mentions légales</li>
-                  </ul>
-                </div>
-                <div>
-                  <h4 className="text-lg font-semibold text-gray-800 mb-4">
-                    Contact
-                  </h4>
-                  <ul className="space-y-3">
-                    <li className="flex items-center text-gray-600">
-                      <Phone size={18} className="mr-2 text-[#E6A4B4]"/>
-                      <a href="tel:+33752081934" className="hover:text-[#E6A4B4] transition-colors">
-                        +33 (0)7 52 08 19 34
-                      </a>
-                    </li>
-                    <li className="flex items-center text-gray-600">
-                      <Mail size={18} className="mr-2 text-[#E6A4B4]"/>
-                      <a href="mailto:contact@velzapp.com" className="hover:text-[#E6A4B4] transition-colors">
-                        contact@velzapp.com
-                      </a>
-                    </li>
-                  </ul>
-                  <div className="mt-4 flex space-x-4">
-                    <a target="_blank"
-                       href="https://instagram.com"
-                       rel="noopener noreferrer"
-                       className="text-[#E6A4B4] hover:text-[#DA70D6] transition-colors">
-                      <Instagram size={24}/>
-                    </a>
-                    <a target="_blank"
-                       href="https://facebook.com"
-                       rel="noopener noreferrer"
-                       className="text-[#E6A4B4] hover:text-[#DA70D6] transition-colors">
-                      <Facebook size={24}/>
-                    </a>
-                    <a target="_blank"
-                       href="https://twitter.com"
-                       rel="noopener noreferrer"
-                       className="text-[#E6A4B4] hover:text-[#DA70D6] transition-colors">
-                      <Twitter size={24}/>
-                    </a>
-                  </div>
-                </div>
-                <div>
-                  <h4 className="text-lg font-semibold text-gray-800 mb-4">
-                    Newsletter
-                  </h4>
-                  <p className="text-gray-600 mb-4">
-                    Restez informé des meilleures offres
-                  </p>
-                  <div className="flex">
-                    <input type="email"
-                           value={newsLetterEmail}
-                           placeholder="Votre email"
-                           onChange={e => setNewsLetterEmail(e.target.value)}
-                           className="flex-1 px-4 py-2 rounded-l-full border border-gray-200 focus:outline-none focus:border-[#E6A4B4]"/>
-                    <button
-                      disabled={invalidNewsLetterEmail}
-                      onClick={e => {
-                        setInvalidNewsLetterEmail(true);
-                        fetch(`/api/newsletter/subscribers`, {
-                          method: 'PATCH',
-                          body: JSON.stringify({email: newsLetterEmail}),
-                        }).then(res => res.json())
-                          .then(({error}) => {
-                            if (error) return;
-                            setNewsLetterEmail('');
-                            const target = e.target as HTMLButtonElement;
-                            target.innerText = 'Voila qui est fait!';
-                            setTimeout(() => target.innerText = 'OK', 1_500)
-                          })
-                      }}
-                      className="px-6 py-2 bg-[#E6A4B4] text-white rounded-r-full hover:bg-[#DA70D6]
-                                 transition-colors disabled:bg-gray-600 whitespace-nowrap">
-                      OK
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <footer className="px-8 w-full bg-white/70 backdrop-blur">
+            <Footer className="mx-auto sm:max-w-2xl lg:max-w-3xl xl:max-w-7xl"/>
           </footer>
+
         </FavouriteDealIdsContext.Provider>
       </SessionContext.Provider>
     </PageSizeContext.Provider>
---
+++ src/common/components/footer.tsx
@@ -0,0 +1,122 @@
+import {useEffect, useState} from "react";
+import {Mail, Phone} from "lucide-react";
+import Link from "next/link";
+import {z} from "zod";
+
+export function Footer({className}: { className?: string }) {
+  const [newsLetterEmail, setNewsLetterEmail] = useState('');
+  const [invalidNewsLetterEmail, setInvalidNewsLetterEmail] = useState(true);
+
+  useEffect(() =>
+    setInvalidNewsLetterEmail(!!z.string().email().safeParse(newsLetterEmail).error), [newsLetterEmail]);
+
+  return <div className={`py-8 grid gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 ${className}`}>
+    <div>
+      <h4 className="mb-2 text-lg font-bold">Nous</h4>
+      <ul className="space-y-2">
+        <li>
+          <Link href="/engagement" className="transition-colors focus:text-secondary hover:text-secondary">
+            Engagement
+          </Link>
+        </li>
+        <li>
+          <Link href="/histoire" className="transition-colors focus:text-secondary hover:text-secondary">
+            Histoire
+          </Link>
+        </li>
+        <li>
+          <Link href="/mission" className="transition-colors focus:text-secondary hover:text-secondary">
+            Mission
+          </Link>
+        </li>
+        <li>
+          <Link href="/vision" className="transition-colors focus:text-secondary hover:text-secondary">
+            Vision
+          </Link>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h4 className="mb-2 text-lg font-bold">Contact</h4>
+      <ul className="space-y-2">
+        <li className="flex items-center">
+          <Phone size={18} className="mr-2 text-secondary"/>
+          <a className="text-nowrap transition-colors whitespace-nowrap focus:text-secondary hover:text-secondary"
+             href="tel:+33752081934">
+            +33 (0)7 52 08 19 34
+          </a>
+        </li>
+        <li className="flex items-center">
+          <Mail size={18} className="mr-2 text-secondary"/>
+          <a className="text-nowrap focus:text-secondary hover:text-secondary transition-colors"
+             href="mailto:contact@velzapp.com">
+            contact@velzapp.com
+          </a>
+        </li>
+      </ul>
+      {/*<div className="mt-4 flex space-x-4">*/}
+      {/*  <a target="_blank"*/}
+      {/*     href="https://instagram.com"*/}
+      {/*     rel="noopener noreferrer"*/}
+      {/*     className="text-[#E6A4B4] hover:text-[#DA70D6] transition-colors">*/}
+      {/*    <Instagram size={24}/>*/}
+      {/*  </a>*/}
+      {/*  <a target="_blank"*/}
+      {/*     href="https://facebook.com"*/}
+      {/*     rel="noopener noreferrer"*/}
+      {/*     className="text-[#E6A4B4] hover:text-[#DA70D6] transition-colors">*/}
+      {/*    <Facebook size={24}/>*/}
+      {/*  </a>*/}
+      {/*  <a target="_blank"*/}
+      {/*     href="https://twitter.com"*/}
+      {/*     rel="noopener noreferrer"*/}
+      {/*     className="text-[#E6A4B4] hover:text-[#DA70D6] transition-colors">*/}
+      {/*    <Twitter size={24}/>*/}
+      {/*  </a>*/}
+      {/*</div>*/}
+    </div>
+    <div>
+      <h4 className="mb-2 text-lg font-bold">Aide</h4>
+      <ul className="space-y-2">
+        <li>
+          <Link href="/cgu" className="transition-colors focus:text-secondary hover:text-secondary">
+            Conditions Générales
+          </Link>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h4 className="mb-2 text-lg font-bold">Newsletter</h4>
+      <p className="mb-2 italic text-sm">
+        Restez informés des meilleures offres
+      </p>
+      <div className="flex">
+        <input type="email"
+               value={newsLetterEmail}
+               placeholder="Votre email"
+               onChange={e => setNewsLetterEmail(e.target.value)}
+               className="flex-1 px-4 py-2 rounded-l-full border border-gray-200 focus:outline-none focus:border-secondary"/>
+        <button
+          disabled={invalidNewsLetterEmail}
+          onClick={e => {
+            setInvalidNewsLetterEmail(true);
+            fetch(`/api/newsletter/subscribers`, {
+              method: 'PATCH',
+              body: JSON.stringify({email: newsLetterEmail}),
+            }).then(res => res.json())
+              .then(({error}) => {
+                if (error) return;
+                setNewsLetterEmail('');
+                const target = e.target as HTMLButtonElement;
+                target.innerText = 'Voila qui est fait!';
+                setTimeout(() => target.innerText = 'OK', 1_500)
+              })
+          }}
+          className="px-6 py-2 bg-secondary text-white rounded-r-full hover:bg-secondary
+                     transition-colors disabled:bg-gray-600 whitespace-nowrap">
+          OK
+        </button>
+      </div>
+    </div>
+  </div>;
+}
